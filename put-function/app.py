6a4e1f38840a255e51ec6dc1561da4d62d753045
bmkr <bfmckeever@gmail.com>
2023-04-09 15:02:37 +1000

    Change function name

--- put-function/app.py
+++ put-function/app.py
@@ -3,7 +3,7 @@ import json
 # import requests
 
 
-def get_function(event, context):
+def put_function(event, context):
     """Sample pure Lambda function
 
     Parameters
@@ -32,19 +32,6 @@ def get_function(event, context):
     #     print(e)
 
     #     raise e
-
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Methods": "*",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "*",
-        },
-        "body": json.dumps({"count": "2"}),
-    }
-
-
-def put_function(event, context):
 
     return {
         "statusCode": 200,
